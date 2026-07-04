0429c2b8537f9e73ab70e8cd5b35e97e1b61a7c0
Theus <matheusth.dev@gmail.com>
2026-07-04 17:19:51 -0300

    ipv4 function in resolver class

--- src/resolver.py
+++ src/resolver.py
@@ -9,3 +9,11 @@ class Resolver:
             print(f"{dom}\t\tIPV6 - {res[2][4][0]}")
         except socket.gaierror:
             None
+
+    @staticmethod
+    def ipv4(dom: str):
+        try:
+            res = socket.getaddrinfo(dom, None, socket.AF_INET)
+            print(f"{dom}\t\tIPV4 - {res[2][4][0]}")
+        except socket.gaierror:
+            None
