d64802b41926b2eb4d9022d6d81992f19f773768
Theus <matheusth.dev@gmail.com>
2026-07-04 17:19:16 -0300

    ipv6 function in resolver class

---
+++ src/resolver.py
@@ -0,0 +1,11 @@
+import socket
+
+
+class Resolver:
+    @staticmethod
+    def ipv6(dom: str):
+        try:
+            res = socket.getaddrinfo(dom, None, socket.AF_INET6)
+            print(f"{dom}\t\tIPV6 - {res[2][4][0]}")
+        except socket.gaierror:
+            None
